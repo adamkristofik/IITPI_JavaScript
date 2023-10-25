9ee9bd1458d7bbc9943a5e935ca01e9c2942e64a
unknown <Adam.Kristofik@sosholic.sk>
2023-10-25 13:11:09 +0200

    1

---
+++ assets/app.js
@@ -0,0 +1,27 @@
+//alert("prvy alert Adam Kristofik");
+
+let prvaPremenna = 3;
+//const druhaPremenna = 7;
+let druhaPremenna = 7;
+
+
+//let vysledok = 3 + 7;
+let vysledok01 = prvaPremenna + druhaPremenna;
+
+alert("3 + 7");
+alert(3 + 7);
+alert();
+alert(prvaPremenna + druhaPremenna);
+alert();
+alert(vysledok01); 
+
+
+
+
+//prvaPremenna = 20;
+
+//const meno = '' ;
+
+//let vysledokScitania = prvaPremenna + druhaPremenna;
+
+//vysledok(meno);
