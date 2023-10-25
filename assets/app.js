794b2ddb39f3ef7782924e21c393dab22d2fb1f7
unknown <Adam.Kristofik@sosholic.sk>
2023-10-25 13:39:29 +0200

    l
    q

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,27 @@
 //alert("prvy alert Adam Kristofik");
 
-let prvaPremenna = 3;
+
 //const druhaPremenna = 7;
-let druhaPremenna = 7;
+let a = 15 + 3 * (4-8/2);
+let b = 150.2/8 *(18,3 -20.1)/3;
+let c = 3**2 + 8 - (31/8);
+alert(a)
+alert(b)
+alert(c)
+
 
 
 //let vysledok = 3 + 7;
-let vysledok01 = prvaPremenna + druhaPremenna;
+//let vysledok01 = prvaPremenna + druhaPremenna;
 
-alert("3 + 7");
-alert(3 + 7);
-alert();
-alert(prvaPremenna + druhaPremenna);
-alert();
-alert(vysledok01); 
+//alert("3 + 7");
+//alert(3 + 7);
+//alert();
+//alert(prvaPremenna + druhaPremenna);
+//alert();
+//alert(vysledok01); 
+
+
 
 
 
